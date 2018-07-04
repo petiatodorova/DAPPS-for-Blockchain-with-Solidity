511599764632e06f5469a4a75e423f79423e5ad9
Petya Todorova <invisioncl@gmail.com>
2018-07-04 09:46:37 +0300

    Instructors.sol changed

--- Instructors.sol
+++ Instructors.sol
@@ -23,15 +23,31 @@ contract Courses {
            instructors[_address].fName = _fName;
            instructors[_address].lName = _lName;
 
-           // Fills up the instructor's array
+           // Fills up instructors array
            InstructorAccounts.push(_address);
        }
 
+       // view all instructoraddresses
        function getInstuctors() public view returns(address[]) {
            return InstructorAccounts;
        }
 
+       // view number of instructors
        function getNumberInstructors() public view returns(uint) {
            return InstructorAccounts.length;
        }
+
+       // view 1 instructor
+       function getOneInstructor(address _address)
+           public
+           view
+           returns(uint, string, string)
+       {
+               return (
+                   instructors[_address].age,
+                   instructors[_address].fName,
+                   instructors[_address].lName
+               );
+       }
+
 }
